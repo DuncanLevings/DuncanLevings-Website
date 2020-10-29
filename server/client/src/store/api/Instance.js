c3c13313d8637901e07a8a926bfe50801f682510
DuncanLevings <levings636>
2020-10-29 19:11:03 -0400

    reworked refresh token interceptor

--- server/client/src/store/api/Instance.js
+++ server/client/src/store/api/Instance.js
@@ -8,34 +8,22 @@ import axios from 'axios';
 import { URL } from "config.js"
 import store from 'store';
 import { getUser, logoutUser } from '../actions/userActions';
+import createAuthRefreshInterceptor from 'axios-auth-refresh';
 
 const axiosInstance = axios.create({
     baseURL: URL
 });
 
-const isHandlerEnabled = (config={}) => {
-    return config.hasOwnProperty('handlerEnabled') && !config.handlerEnabled ? false : true
-}
+const refreshAuthLogic = () =>
+    axiosInstance.get('/api/users/refresh-token')
+        .then(() => {
+            store.dispatch(getUser());
+            return Promise.resolve();
+        })
+        .catch(() => {
+            store.dispatch(logoutUser());
+        });
 
-const errorHandler = (error) => {
-    if (isHandlerEnabled(error.config)) {
-        if (error.response.status === 401 && !error.config._retry) {
-            error.config._retry = true;
-            axiosInstance.get("/api/users/refresh-token").then(() => {
-                store.dispatch(getUser());
-                return axios(error.config);
-            })
-            .catch(() => {
-                store.dispatch(logoutUser());
-            });
-        }
-    }
-    return Promise.reject({ ...error });
-}
-
-axiosInstance.interceptors.response.use(
-    response => { return response; },
-    error => errorHandler(error)
-);
+createAuthRefreshInterceptor(axiosInstance, refreshAuthLogic);
 
 export default axiosInstance;
